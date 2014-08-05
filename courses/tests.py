1635202696ba0c46c129d12da540923b6bc876ad
Ross Brunton <bruntonross@gmail.com>
2014-08-05 14:35:31 +0100

    Unit tests for autoslug

--- courses/tests.py
+++ courses/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Course, Lesson, Section, Task
+
+class CoursesTestCase(TestCase):
+    def test_autoslug(self):
+        """Is the slug field set correctly"""
+        c = Course(title="Hello World!")
+        c.save()
+        self.assertEqual(c.slug, "hello-world")
+        
+        l = Lesson(title="Hello World!", course=c)
+        l.save()
+        self.assertEqual(l.slug, "hello-world")
+        
+        s = Section(title="Hello World!", lesson=l)
+        s.save()
+        self.assertEqual(s.slug, "hello-world")
+        
+        s.title = "Goodbye World!"
+        s.save()
+        self.assertEqual(s.slug, "goodbye-world")
