2af67c461e194724382a1c27556355ded271ebcc
Ross Brunton <bruntonross@gmail.com>
2014-08-06 15:16:42 +0100

    Basic HTML templates and styles
    Also course list page

--- courses/views.py
+++ courses/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from courses.models import Course
+
 @login_required
 def index(request):
-    return render(request, "courses/index.html", {})
+    ctx = {}
+    ctx["courses"] = Course.get_courses(request.user);
+    
+    return render(request, "courses/index.html", ctx)
 
 def course(request, course):
     ...
--- static/scripts.js
+++ static/scripts.js
@@ -1,1 +1,14 @@
 "use strict";
+
+window.rthing = (function() {
+    var rthing = {};
+    
+    $(document).ready(function() {
+        // Put up with IE10
+        if(/*@cc_on!@*/false) {
+            $("body").addClass("ie10");
+        }
+    });
+    
+    return rthing;
+})();
