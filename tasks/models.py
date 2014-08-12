729d1ab6609211a9e4757efadea5088f5f7ae994
Ross Brunton <bruntonross@gmail.com>
2014-08-12 16:58:39 +0100

    Basic models for user stats tracking
    They have nothing in them yet.

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.template import defaultfilters
 
-# Create your models here.
+from courses.models import Task
+
+class UserTaskStatusModel(models.Model):
+    user = models.ForeignKey(User, related_name="uts")
+    task = models.ForeignKey(Task, related_name="uts")
--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class UserExtraData(models.Model):
+    user = models.OneToOneField(User, related_name="extra")
+
+@receiver(post_save, sender=User)
+def _user_created(sender, instance, created, **kwargs):
+    if created:
+        UserExtraData.objects.get_or_create(user=instance)
