41132c1cc850e619627e46e8caef7af25ef9338d
Ross Brunton <bruntonross@gmail.com>
2014-08-20 13:16:07 +0100

    Course Editor user group

--- staff/admin.py
+++ staff/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin, GroupAdmin
+from django.contrib.auth.models import User, Group, Permission
 
 class StaffAdminSite(admin.sites.AdminSite):
     site_title = "Rthing Administration"
@@ -10,3 +10,4 @@ class StaffAdminSite(admin.sites.AdminSite):
 admin_site = StaffAdminSite()
 
 admin_site.register(User, UserAdmin)
+admin_site.register(Group, GroupAdmin)
---
+++ staff/management.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import User, Group, Permission
+from django.db.models import signals
+from django.dispatch import receiver
+
+from courses.models import Course, Lesson, Section, Task
+import courses.models as models
+
+@receiver(signals.post_syncdb)
+def _post_syncdb(sender, **kwargs):
+    """Creates the "Course Editor" group.
+    
+    sender is the model that was created
+    """
+    
+    if sender != models:
+        return
+    
+    group = Group.objects.get_or_create(name="Course Editor")[0]
+    
+    permissions = [
+        "{}_{}".format(x, y)
+        for x in ["add", "change", "delete"]
+        for y in ["course", "lesson", "section", "task"]
+    ]
+    
+    for p in permissions:
+        group.permissions.add(Permission.objects.get(codename=p))
