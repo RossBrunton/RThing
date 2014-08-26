4663228147be4a1a6096cddc144d260c405079c9
Ross Brunton <bruntonross@gmail.com>
2014-08-26 16:18:00 +0100

    Some tests for stats

--- stats/tests.py
+++ stats/tests.py
@@ -1,3 +1,54 @@
-from django.test import TestCase
+from django.contrib.auth.models import User, Permission
+from django.test import TestCase, Client
 
-# Create your tests here.
+from courses.models import Course, Lesson, Section, Task
+from stats import utils
+from stats.models import UserOnTask
+
+class StatsTestCase(TestCase):
+    f = None
+    m = None
+    t = None
+    muot = None
+    fuot = None
+    
+    def setUp(self):
+        self.m = User.objects.create_user("Mittens", "mittensthekitten@gmail.com", "meow")
+        self.m.save()
+        self.f = User.objects.create_user("Fido", "fido@gmail.com", "woof")
+        self.f.save()
+        
+        c = Course()
+        c.save()
+        l = Lesson(course=c)
+        l.save()
+        s = Section(lesson=l)
+        s.save()
+        self.t = Task(section=s, model_answer="replaced", language="dummy")
+        self.t.save()
+        
+        self.muot = self.t.get_uot(self.m)
+        self.fuot = self.t.get_uot(self.f)
+    
+    def test_stats(self):
+        """Tests whether the utility functions work"""
+        self.assertEqual(utils.attempts(task=self.t), 0)
+        self.assertEqual(utils.correct(task=self.t), 0)
+        self.assertEqual(utils.revealed(task=self.t), 0)
+        self.assertEqual(utils.average_tries_correct(task=self.t), 0.0)
+        self.assertEqual(utils.average_tries_reveal(task=self.t), 0.0)
+        self.assertEqual(utils.completion(task=self.t), 0.0)
+        
+        self.muot.tries += 1
+        self.muot.save()
+        self.fuot.tries += 5
+        self.fuot.save()
+        
+        self.assertEqual(utils.attempts(task=self.t), 2)
+        self.assertEqual(utils.correct(task=self.t), 0)
+        self.assertEqual(utils.revealed(task=self.t), 0)
+        self.assertEqual(utils.average_tries_correct(task=self.t), 0.0)
+        self.assertEqual(utils.average_tries_reveal(task=self.t), 0.0)
+        self.assertEqual(utils.completion(task=self.t), 0.0)
+        
+        
