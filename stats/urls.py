6b3dbd9e31f0d420440cf90865d1c9d9514fbc26
Ross Brunton <bruntonross@gmail.com>
2014-08-26 16:17:32 +0100

    Started work on "show stats for user" page

--- stats/urls.py
+++ stats/urls.py
@@ -5,4 +5,5 @@ import stats.views as views
 urlpatterns = patterns('',
     url(r"^(?P<course>[a-z0-9-]+)/(?P<lesson>[a-z0-9-]+)$", views.lesson, name="lesson"),
     url(r"^~wrong/(?P<task>[0-9]+)$", views.wrong, name="wrong"),
+    url(r"^(?P<course>[a-z0-9-]+)/(?P<lesson>[a-z0-9-]+)/user/(?P<name>[0-9a-zA-Z_@.+-]+)$", views.user, name="user"),
 )
