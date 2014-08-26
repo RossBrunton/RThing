0651ad86809e65d447c0551e151a5203de991544
Ross Brunton <bruntonross@gmail.com>
2014-08-26 13:05:48 +0100

    More unit tests for tasks

--- tasks/tests.py
+++ tasks/tests.py
@@ -6,31 +6,40 @@ from tasks import utils
 
 class TasksTestCase(TestCase):
     lang = "dummy"
+    s = None
+    u = None
+    
+    def setUp(self):
+        self.u = User.objects.create_user("Mittens", "mittensthekitten@gmail.com", "meow")
+        self.u.save()
+        
+        c = Course()
+        c.save()
+        l = Lesson(course=c)
+        l.save()
+        self.s = Section(lesson=l)
+        self.s.save()
     
     def test_perform_execute(self):
         """Minimum functionality of utils.perform_execute"""
-        u = User.objects.create_user("Mittens", "mittensthekitten@gmail.com", "meow")
-        
-        c = Course()
-        l = Lesson(course=c)
-        s = Section(lesson=l)
-        t = Task(section=s, model_answer="replaced", language=self.lang)
+        t = Task(section=self.s, model_answer="replaced", language=self.lang)
+        t.save()
         
         # Remember dummy replaces "replace_me" with "replaced"
         
-        (out, media, is_error, is_correct) = utils.perform_execute("replaced", t, u)
+        (out, media, is_error, is_correct) = utils.perform_execute("replaced", t, self.u)
         
         self.assertEqual(out, "replaced")
         self.assertTrue(is_correct)
         self.assertFalse(is_error)
         
-        (out, media, is_error, is_correct) = utils.perform_execute("nope", t, u)
+        (out, media, is_error, is_correct) = utils.perform_execute("nope", t, self.u)
         
         self.assertEqual(out, "nope")
         self.assertFalse(is_correct)
         self.assertFalse(is_error)
         
-        (out, media, is_error, is_correct) = utils.perform_execute("replace_me", t, u)
+        (out, media, is_error, is_correct) = utils.perform_execute("%replace", t, self.u)
         
         self.assertEqual(out, "replaced")
         self.assertTrue(is_correct)
@@ -39,28 +48,24 @@ class TasksTestCase(TestCase):
     
     def test_no_automark(self):
         """Test tasks with automark being false"""
-        u = User.objects.create_user("Mittens", "mittensthekitten@gmail.com", "meow")
-        
-        c = Course()
-        l = Lesson(course=c)
-        s = Section(lesson=l)
-        t = Task(section=s, model_answer="replaced", language=self.lang, automark=False)
+        t = Task(section=self.s, model_answer="replaced", language=self.lang, automark=False)
+        t.save()
         
         # Remember dummy replaces "replace_me" with "replaced"
         
-        (out, media, is_error, is_correct) = utils.perform_execute("replaced", t, u)
+        (out, media, is_error, is_correct) = utils.perform_execute("replaced", t, self.u)
         
         self.assertEqual(out, "replaced")
         self.assertFalse(is_correct)
         self.assertFalse(is_error)
         
-        (out, media, is_error, is_correct) = utils.perform_execute("nope", t, u)
+        (out, media, is_error, is_correct) = utils.perform_execute("nope", t, self.u)
         
         self.assertEqual(out, "nope")
         self.assertFalse(is_correct)
         self.assertFalse(is_error)
         
-        (out, media, is_error, is_correct) = utils.perform_execute("replace_me", t, u)
+        (out, media, is_error, is_correct) = utils.perform_execute("%replace", t, self.u)
         
         self.assertEqual(out, "replaced")
         self.assertFalse(is_correct)
@@ -69,33 +74,66 @@ class TasksTestCase(TestCase):
     
     def test_pre_post(self):
         """Test to see if pre/post code is handled correctly"""
-        u = User.objects.create_user("Mittens", "mittensthekitten@gmail.com", "meow")
-        
-        c = Course()
-        l = Lesson(course=c)
-        s = Section(lesson=l)
-        t = Task(section=s, model_answer="replaced", language=self.lang, automark=False,
+        t = Task(section=self.s, model_answer="replaced", language=self.lang, automark=False,
             hidden_pre_code="A", visible_pre_code="B", post_code="Z"
         )
+        t.save()
         
-        # Remember dummy replaces "replace_me" with "replaced"
+        # Remember dummy replaces "%replace" with "replaced"
         
-        (out, media, is_error, is_correct) = utils.perform_execute("replaced", t, u)
+        (out, media, is_error, is_correct) = utils.perform_execute("replaced", t, self.u)
         
-        self.assertEqual(out, "A\nB\nreplaced")
+        self.assertEqual(out, "ABreplaced")
         self.assertFalse(is_correct)
         self.assertFalse(is_error)
         
-        (out, media, is_error, is_correct) = utils.perform_execute("nope", t, u)
+        (out, media, is_error, is_correct) = utils.perform_execute("nope", t, self.u)
         
-        self.assertEqual(out, "A\nB\nnope")
+        self.assertEqual(out, "ABnope")
         self.assertFalse(is_correct)
         self.assertFalse(is_error)
         
-        (out, media, is_error, is_correct) = utils.perform_execute("replace_me", t, u)
+        (out, media, is_error, is_correct) = utils.perform_execute("%replace", t, self.u)
         
-        self.assertEqual(out, "A\nB\nreplaced")
+        self.assertEqual(out, "ABreplaced")
         self.assertFalse(is_correct)
         self.assertFalse(is_error)
+
+    def test_random(self):
+        """Test to see if uses_random tasks are handled correctly"""
+        t = Task(section=self.s, model_answer="%seed", language=self.lang, uses_random=True)
+        t.save()
         
+        # Remember dummy replaces "%replace" with "replaced"
         
+        (out, media, is_error, is_correct) = utils.perform_execute("%seed", t, self.u)
+        
+        self.assertTrue(is_correct)
+    
+    def test_takes_prior(self):
+        """Test to see if takes_prior tasks are handled correctly"""
+        a = Task(section=self.s, model_answer="replaced", hidden_pre_code="a", language=self.lang)
+        a.save()
+        b = Task(section=self.s, model_answer="replaced", language=self.lang, takes_prior=True)
+        b.save()
+        
+        (out, media, is_error, is_correct) = utils.perform_execute("%replace", a, self.u)
+        self.assertEqual(out, "areplaced")
+        self.assertTrue(is_correct)
+        
+        (out, media, is_error, is_correct) = utils.perform_execute("%replace", b, self.u)
+        self.assertEqual(out, "replaced")
+        self.assertTrue(is_correct)
+        
+    def test_takes_prior_random(self):
+        """Test to see if takes_prior tasks with random seeds are handled correctly"""
+        a = Task(section=self.s, model_answer="replaced", hidden_pre_code="%seed", language=self.lang, uses_random=True)
+        a.save()
+        b = Task(section=self.s, model_answer="replaced", language=self.lang, takes_prior=True)
+        b.save()
+        
+        (out, media, is_error, is_correct) = utils.perform_execute("%replace", a, self.u)
+        self.assertTrue(is_correct)
+        
+        (out, media, is_error, is_correct) = utils.perform_execute("%replace", b, self.u)
+        self.assertTrue(is_correct)
