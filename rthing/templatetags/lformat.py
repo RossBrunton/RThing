ad82333b64562ccf26691d2f8b39433018093212
Ross Brunton <bruntonross@gmail.com>
2014-08-28 13:35:46 +0100

    youtube, html and url formatting

--- rthing/templatetags/lformat.py
+++ rthing/templatetags/lformat.py
@@ -4,10 +4,23 @@ from django.utils.safestring import mark_safe
 
 from courses.models import Lesson
 import settings
+from six.moves.html_parser import HTMLParser
 
 import re
 
 register = template.Library()
+
+_YOUTUBE_IFRAME = r"""
+<iframe type='text/html' width='640' height='385' src='http://www.youtube.com/embed/\1' allowfullscreen frameborder='0'
+sandbox="allow-scripts allow-same-origin" class='youtube-iframe'
+>
+</iframe>
+"""
+
+def _reverse_escape(match):
+    """Undos the escaping"""
+    return (HTMLParser()).unescape(match.group(1))
+    
 
 @register.filter(needs_autoescape=True)
 def lformat(value, arg=None, autoescape=None):
@@ -43,7 +56,17 @@ def lformat(value, arg=None, autoescape=None):
             "<img src='{}{}/{}' alt='{}'>".format(settings.MEDIA_URL, arg, esc(match.group(3)), esc(match.group(1)))
         )
     
-    value = re.sub("\[img\s*(.*?)\](.*?//)?(.*)\[/img\]", image_repl, value)
+    value = re.sub(r"\[img\s*(.*?)\](.*?//)?(.*?)\[/img\]", image_repl, value)
+    
+    # Raw HTML
+    value = re.sub(r"\[html\](.*?)\[\/html\]", _reverse_escape, value)
+    
+    # Youtube
+    value = re.sub(r"\[youtube\](.+?)\[\/youtube\]", _YOUTUBE_IFRAME, value)
+    
+    # Links
+    value = re.sub(r"\[url\](.*?)\[/url\]", r"<a href='\1'>\1</a>", value)
+    value = re.sub(r"\[url\s*(.*?)\](.*?)\[/url\]", r"<a href='\1'>\2</a>", value)
     
     # Boxes
     value = re.sub(r"\<div\>[Ww]arning:(.+?)\<\/div\>", r"<div class='l-warning'>\1</div>", value)
