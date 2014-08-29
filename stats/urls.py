1322548fe318417557f8726eee552150985c6a15
Ross Brunton <bruntonross@gmail.com>
2014-08-29 13:14:14 +0100

    CSV export

--- stats/urls.py
+++ stats/urls.py
@@ -6,4 +6,7 @@ urlpatterns = patterns('',
     url(r"^(?P<course>[a-z0-9-]+)/(?P<lesson>[a-z0-9-]+)$", views.lesson, name="lesson"),
     url(r"^~wrong/(?P<task>[0-9]+)$", views.wrong, name="wrong"),
     url(r"^(?P<course>[a-z0-9-]+)/(?P<lesson>[a-z0-9-]+)/user/(?P<name>[0-9a-zA-Z_@.+-]+)$", views.user, name="user"),
+    
+    url(r"^csv/(?P<course>[a-z0-9-]+)/(?P<lesson>[a-z0-9-]+)$", views.csv_lesson, name="csv_lesson"),
+    url(r"^csv/(?P<course>[a-z0-9-]+)/(?P<lesson>[a-z0-9-]+)/users$",views.csv_users, name="csv_users"),
 )
--- stats/views.py
+++ stats/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.core.exceptions import SuspiciousOperation
-from django.http import Http404
+from django.http import Http404, HttpResponse
 from django.views.decorators.http import require_POST
 from django.contrib.auth.models import User
 
-from courses.models import Course, Lesson, Section, Task
+from StringIO import StringIO
+import csv
 
+from courses.models import Course, Lesson, Section, Task
 from stats import utils
 
 @login_required
@@ -39,6 +41,33 @@ def lesson(request, course, lesson):
     ]
     
     return render(request, "stats/lesson.html", ctx)
+
+
+@login_required
+@user_passes_test(lambda u: u.is_staff)
+def csv_lesson(request, course, lesson):
+    course = get_object_or_404(Course, slug=course)
+    lesson = get_object_or_404(Lesson, slug=lesson, course=course)
+    
+    buffer = StringIO()
+    writer = csv.writer(buffer)
+    
+    # Headers
+    writer.writerow(
+        ["Section", "Task", "Attempts", "Correct", "Revealed"]
+    )
+    
+    section_number = 0
+    for s in lesson.sections.all():
+        section_number += 1
+        task_number = 0
+        for t in s.tasks.all():
+            task_number += 1
+            writer.writerow(
+                [section_number, task_number, utils.attempts(task=t), utils.correct(task=t), utils.revealed(task=t)]
+            )
+    
+    return HttpResponse(buffer.getvalue(), "text/csv")
 
 
 @login_required
@@ -93,3 +122,31 @@ def user(request, name, course, lesson):
     
     
     return render(request, "stats/user.html", ctx)
+
+
+@login_required
+@user_passes_test(lambda u: u.is_staff)
+def csv_users(request, course, lesson):
+    course = get_object_or_404(Course, slug=course)
+    lesson = get_object_or_404(Lesson, slug=lesson, course=course)
+    
+    buffer = StringIO()
+    writer = csv.writer(buffer)
+    
+    # Headers
+    writer.writerow(
+        ["Username", "Attempts", "Correct", "Revealed"]
+    )
+    
+    users = course.users.all()
+    
+    for u in users:
+        writer.writerow([
+            u.username,
+            utils.attempts(user=u, task__section__lesson=lesson),
+            utils.correct(user=u, task__section__lesson=lesson),
+            utils.revealed(user=u, task__section__lesson=lesson),
+        ])
+    
+    
+    return HttpResponse(buffer.getvalue(), "text/csv")
