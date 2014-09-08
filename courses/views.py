c3c2d1224315f712b100d58ebc34f72fb3196ea2
Ross Brunton <bruntonross@gmail.com>
2014-09-08 17:59:46 +0100

    Redesigned the sidebar

--- courses/views.py
+++ courses/views.py
@@ -59,6 +59,7 @@ def lesson(request, course, lesson):
     ctx["course"] = get_object_or_404(Course, slug=course)
     ctx["lesson"] = get_object_or_404(Lesson, slug=lesson, course=ctx["course"])
     ctx["all_lessons"] = filter(lambda l : l.can_see(request.user), ctx["course"].lessons.all())
+    ctx["all_courses"] = filter(lambda l : l.can_see(request.user), Course.objects.all())
     
     # Check user has permission
     if not ctx["lesson"].can_see(request.user):
